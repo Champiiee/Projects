cb5969ab3015c0361e8e08bf94db025a8fca5bb8
Kimi Brent Mendoza <kimi.brent08@gmail.com>
2025-03-05 11:36:50 +0800

    Update HomePage.js

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, TableBody } from "@mui/material";
 import { Link } from "react-router-dom";
 import homeImage from "../assets/img.png"; // Import the image
 
 function HomePage() {
+  const body = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "100vh",
+    padding: "20px",
+  };
+  const innerBody = {
+    padding: "30px",
+    borderRadius: "10px",
+    width: "100%",
+    maxWidth: "500px",
+    backgroundColor: "#fff",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+    textAlign: "center",
+  };
   return (
     <div
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        padding: "20px",
+        ...body
       }}
     >
       <div
         style={{
-          padding: "30px",
-          borderRadius: "10px",
-          width: "100%",
-          maxWidth: "500px",
-          backgroundColor: "#fff",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          textAlign: "center",
+          ...innerBody,
         }}
       >
         <Typography variant="h4" gutterBottom>
